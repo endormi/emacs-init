dfb5893ee2855e9317847f1cb59e7421ceaae36c
endormi <ernosalo14@gmail.com>
2021-05-03 22:58:10 +0300

    Add init.el
    Will be updated a lot

---
+++ init.el
@@ -0,0 +1,46 @@
+;;; This my init.el file
+;;; Emacs 26
+;;; Not compatible with Emacs 25 or lower
+
+;;; TODO
+
+(require 'package)
+(package-initialize)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")))
+;;; My custom file
+(setq custom-file "~/.emacs.d/custom.el")
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package markdown-mode
+  :ensure t
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+(setq create-lockfiles nil)
+(setq kill-do-not-save-duplicates t)
+(setq ring-bell-function 'ignore)
+
+(scroll-bar-mode 0)
+(menu-bar-mode 0)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+(dolist (mode '(shell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
