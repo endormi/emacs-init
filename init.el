dfb631f4db9af5d5c1fd0e6938b5cfb8769878a2
endormi <ernosalo14@gmail.com>
2021-09-11 13:15:07 +0300

    Remove kill-line and update yank key
    * Kill-Line is already C-k (so no need to over complicate keys) and C-v is taken so C-c C-v is good.

--- init.el
+++ init.el
@@ -86,8 +86,7 @@
 (global-set-key (kbd "C-x M-z") 'kill-all-buffers)
 (global-set-key (kbd "C-c C-d") 'duplicate-line)
 (global-set-key (kbd "C-c M-f") 'open-new-frame-horizontally)
-(global-set-key (kbd "C-x C-M-k") 'kill-line)
-(global-set-key (kbd "C-c C-M-v") 'yank)
+(global-set-key (kbd "C-c C-v") 'yank)
 
 (use-package markdown-mode
   :ensure t
