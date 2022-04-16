1cd26cf0f9357d940b433ecae6bd17237af1f5eb
endormi <ernosalo14@gmail.com>
2022-04-16 21:06:22 +0300

    Remove she-bang function

--- init.el
+++ init.el
@@ -78,18 +78,6 @@
   (let ((fill-column (point-max))) (fill-paragraph nil)))
 
 ;;; TODO:
-;;; Simple function to add a she-bang to first line
-(defun she-bang(lang)
-  "Add she-bang with user prompt."
-  (interactive "sEnter language: ")
-  (beginning-of-buffer)
-  (newline)
-  (newline)
-  (beginning-of-buffer)
-  ;;; insert needs to be fixed
-  (insert "" lang))
-
-;;; TODO:
 ;;; kbds can be updated later, depending on what I might add later
 (global-set-key (kbd "C-x 1") 'beginning-of-buffer)
 (global-set-key (kbd "C-x 0") 'end-of-buffer)
@@ -104,7 +92,6 @@
 (global-set-key (kbd "C-c C-d") 'duplicate-line)
 (global-set-key (kbd "C-c M-f") 'open-new-frame-horizontally)
 (global-set-key (kbd "C-c C-v") 'yank)
-;;; (global-set-key (kbd "C-j") 'she-bang)
 (global-set-key (kbd "<f5>") 'unfill-paragraph)
 
 (global-unset-key (kbd "<insert>"))
